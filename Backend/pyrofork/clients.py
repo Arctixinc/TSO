da0a25dd20a6ef88dba67b0d122339ede5666ad8
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2025-10-25 12:09:42 +0530

    Fix client name formatting in initialization

--- Backend/pyrofork/clients.py
+++ Backend/pyrofork/clients.py
@@ -26,7 +26,7 @@ async def start_client(client_id: int, token: str) -> Optional[Tuple[int, Client
         LOGGER.info(f"[Client {client_id}] Starting initialization...")
         c_name =str(client_id),
         client = Client(
-            name=f"sessions/{c_name}"
+            name=f"sessions/{c_name}",
             api_id=Telegram.API_ID,
             api_hash=Telegram.API_HASH,
             bot_token=token,
